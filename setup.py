956a6c155fe41530665de672df7b8f5a01689eb2
thecodeingfreakj <pallavidapriya75@gmail.com>
2023-04-29 04:16:17 +0530

    Latest Commit

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ CLASSIFIERS = [
 ]
 
 # calling the setup function
-setup(name='passwordmanager3',
+setup(name='LassPass3',
       version='1.0.0',
       description='A simple python wrapper for creating passwords..',
       long_description="A simple python wrapper for creating passwords",
@@ -34,7 +34,7 @@ setup(name='passwordmanager3',
       author_email='pallavidapriya75@gmail.com',
       license='MIT',
       # packages=[ 'password-manager' ],
-      packages=find_packages('passwordmanager3'),
+      packages=find_packages('LassPass3'),
       classifiers=CLASSIFIERS,
       install_requires=REQUIREMENTS,
       keywords='password manager'
